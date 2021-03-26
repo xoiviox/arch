eaade499f6f66d121f3ad17152de125f8d22df17
enerke <enerke@protonmail.com>
2021-03-26 16:46:30 +0100

    SE

---
+++ 00_hand.sh
@@ -0,0 +1,9 @@
+loadkeys pl
+timedatectl set-ntp true
+fdisk /dev/sda
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+reflector -c pl
+pacstrap /mnt base linux linux-firmware
+genfstab -L /mnt >> /mnt/etc/fstab			### -L for label, -U for UUID
+arch-chroot /mnt
