7765a397ea721b79ffa64200d1ba918f1218e1a0
enerke <enerke@protonmail.com>
2021-03-26 19:39:25 +0100

    work in progress

--- 01_install.sh
+++ 01_install.sh
@@ -7,7 +7,7 @@ hwclock --systohc
 echo '############ setting locale'
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' '/etc/locale.gen'
 sed -i 's/#pl_PL.UTF-8 UTF-8/pl_PL.UTF-8 UTF-8/g' '/etc/locale.gen'
-locale-gen
+locale-gen >> '/dev/null'
 echo 'LANG=pl_PL.UTF-8' >> '/etc/locale.conf'
 echo 'KEYMAP=pl' >> '/etc/vconsole.conf'
 
