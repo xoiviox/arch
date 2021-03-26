4d0b30384225ee2bfa9b883b0369826292264afb
enerke <enerke@protonmail.com>
2021-03-26 19:38:13 +0100

    work in progress

--- 01_install.sh
+++ 01_install.sh
@@ -32,8 +32,8 @@ echo ''
 echo -e '$USERPASSWORD\n$USERPASSWORD' | passwd $USERNAME
 
 echo '############ installing needed software'
-reflector -c pl
-pacman --noconfirm -Sq wget nano grub openssh sudo git
+pacman --noconfirm -Sq nano grub openssh sudo git networkmanager reflector >> '/dev/null'
+reflector -c pl 
 
 echo '############ setting up sshd'
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' '/etc/ssh/sshd_config'
@@ -45,8 +45,9 @@ echo '############ enabling networkmanager'
 systemctl enable NetworkManager
 
 echo '############ setting up grub'
-echo -e '\nGRUB_FORCE_HIDDEN_MENU="true"' | tee -a '/etc/default/grub'
-wget 'https://gist.githubusercontent.com/anonymous/8eb2019db2e278ba99be/raw/257f15100fd46aeeb8e33a7629b209d0a14b9975/gistfile1.sh' -O '/etc/grub.d/31_hold_shift'
+echo -e '\nGRUB_FORCE_HIDDEN_MENU="true"' >> '/etc/default/grub'
+curl -s 'https://gist.githubusercontent.com/anonymous/8eb2019db2e278ba99be/raw/257f15100fd46aeeb8e33a7629b209d0a14b9975/gistfile1.sh' -o '/etc/grub.d/31_hold_shift'
 chmod a+x '/etc/grub.d/31_hold_shift'
 grub-mkconfig -o '/boot/grub/grub.cfg'
-grub-install '/dev/sda'
+read -p '### enter device to install grub (eg /dev/sda): ' DEVICE
+grub-install $DEVICE
