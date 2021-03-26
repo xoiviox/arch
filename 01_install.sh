bce087b48a0bbe44be27af697bd4e9dc0043ff59
enerke <enerke@protonmail.com>
2021-03-26 17:03:00 +0100

    fixed

--- 01_install.sh
+++ 01_install.sh
@@ -1,1 +1,7 @@
-
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#pl_PL.UTF-8 UTF-8/pl_PL.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+echo 'LANG=pl_PL.UTF-8' >> /etc/locale.conf
+echo 'KEYMAP=pl' >> /etc/vconsole.conf
