7e8705406b6ce0e200c220ffdcb25958755eeba1
enerke <enerke@protonmail.com>
2021-03-26 20:15:18 +0100

    work in progress

--- 01_install.sh
+++ 01_install.sh
@@ -48,6 +48,7 @@ echo '############ setting up grub'
 echo -e '\nGRUB_FORCE_HIDDEN_MENU="true"' >> '/etc/default/grub'
 curl -s 'https://gist.githubusercontent.com/anonymous/8eb2019db2e278ba99be/raw/257f15100fd46aeeb8e33a7629b209d0a14b9975/gistfile1.sh' -o '/etc/grub.d/31_hold_shift'
 chmod a+x '/etc/grub.d/31_hold_shift'
+mkdir /boot/grub
 grub-mkconfig -o '/boot/grub/grub.cfg'
 read -p '### enter device to install grub (eg /dev/sda): ' DEVICE
 grub-install $DEVICE >> '/dev/null'
